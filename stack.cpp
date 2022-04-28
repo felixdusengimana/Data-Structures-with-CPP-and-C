37756120a783745263f03210f8a25393ff275285
phelixdusengimana <phelixdusengimana@gmail.com>
2022-04-28 21:13:44 +0200

    feat: stack implementation

---
+++ stack.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int MAX_SIZE = 10
+
+
+class Stack
+{
+public:
+
+	void push(int elem){
+		
+	}
+
+	
+};
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
